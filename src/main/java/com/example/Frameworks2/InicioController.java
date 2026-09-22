73df207e6986682c7f1db8d3cc36ba4ab9480eb6
Luana Bosco Guidine <luanaguidine96@gmail.com>
2026-09-22 07:30:16 -0300

    Add InicioController for handling root requests

---
+++ src/main/java/com/example/Frameworks2/InicioController.java
@@ -0,0 +1,13 @@
+package com.example.Frameworks2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InicioController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "index.html";
+    }
+}
